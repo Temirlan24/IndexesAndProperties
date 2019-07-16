cd2e47edf6e961f367b59dfb110f4262dcdf692f
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-16 17:00:59 +0600

    Сделал практическую работу, создал класс и реализовал в Program

---
+++ PracticWork/PracticWork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите индексный диапазон (начало): ");
+            int.TryParse(Console.ReadLine(), out int lowIndex);
+            Console.WriteLine("Введите индексный диапазон (конец): ");
+            int.TryParse(Console.ReadLine(), out int highIndex);
+            Random random = new Random();
+            if (lowIndex < highIndex)
+            {
+                Console.WriteLine("Нижний индекс = {0}. Вверхний индекс = {1}", lowIndex, highIndex);
+                RangeOfArray range = new RangeOfArray(lowIndex, highIndex);
+                for (int i = 0; i < range.array.Length; i++)
+                {
+                    range.array[i] = random.Next(1, 20);
+                }
+                range.ShowIndexation(range, lowIndex);
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные индексы!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ PracticWork/PracticWork/RangeOfArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticWork
+{
+     public class RangeOfArray
+    {
+        public int startIndex;
+        public int endIndex;
+        public int[] array;
+        public RangeOfArray(int startIndex, int endIndex)
+        {
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+            array = new int[Math.Abs(endIndex - startIndex) + 1];
+        }
+        public void ShowIndexation(RangeOfArray massive, int startIndex)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("array[{0}] элемент массива равен = " + array[i], (i + startIndex));
+            }
+        }
+    }
+}
