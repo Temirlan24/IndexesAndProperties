b133c330351f0019cca49fd41d4c9469b813cdd4
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-17 15:08:52 +0600

    Сделал Домашнюю работу по Индексаторам и свойствам. Создал класс которая возводит в квадрат значение массива.

---
+++ HomeWork/HomeWork/PowArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class PowArray
+    {
+        private int[] array;
+        public int SizeArray { get; set; }
+        public PowArray(int sizeArray)
+        {
+            this.SizeArray = sizeArray;
+            array = new int[SizeArray];
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return this.array[i];
+            }
+            set
+            {
+                this.array[i] = (int)Math.Pow(value, 2);
+            }
+        }
+    }
+}
---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowArray array = new PowArray(3);
+            array[0] = 2;
+            array[1] = 4;
+            array[2] = 10;
+            for(int i = 0; i < array.SizeArray; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
